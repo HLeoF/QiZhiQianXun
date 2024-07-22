4c11b3d06acb94e26ebad46f67c3a2fa68d672c7
Maiqi Hou <Maiqi_Hou1@baylor.edu>
2024-07-22 17:19:22 -0600

    new

--- LangChain/LangChain.py
+++ LangChain/LangChain.py
@@ -2,12 +2,6 @@ from langchain_community.chat_models import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
-system_prompt = (
-    "你是一个诗人，具体的写诗风格根据用户的要求。"
-    "如果用户的要求中没有提到过 风格 二字，就当作用户没有提要求，并回答你不能够写"
-    "如果用户提及到的诗人并不是属于中国/中国古时候的朝代，或者诗人来自其他国家，你就说告诉一个中国诗人"
-    "诗的格式只能是4句话"
-)
 
 prompt = ChatPromptTemplate.from_template(
     """你是一个诗人，可以根据用户的要求写诗。请根据以下规则来作诗：
